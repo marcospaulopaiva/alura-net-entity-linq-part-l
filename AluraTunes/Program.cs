fc09b9030debec202b82317db2c17ebe598f1b21
Marcos Paulo <marcos.poo@gmail.com>
2024-07-29 18:20:16 -0300

    Aula 1.1 -  Introdução ao Linq com select from e where

--- AluraTunes/Program.cs
+++ AluraTunes/Program.cs
@@ -10,6 +10,46 @@ namespace AluraTunes
     {
         static void Main(string[] args)
         {
+            //lista os gêneros rock
+            var generos = new List<Genero>
+            {
+                new Genero { Id = 1, Nome = "Rock"},
+                new Genero { Id = 2, Nome = "Reggae"},
+                new Genero { Id = 3, Nome = "Rock Progressivo"},
+                new Genero { Id = 4, Nome = "Punk Rock"},
+                new Genero { Id = 5, Nome = "Clássica"}
+            };
+
+            foreach (var genero in generos) 
+            {
+                if (genero.Nome.Contains("Rock"))
+                {
+                    Console.WriteLine("{0}\t{1}",genero.Id, genero.Nome);
+                }
+            }
+
+            Console.WriteLine();
+
+            //select * from generos
+
+            var query = from g in generos
+                        where g.Nome.Contains("Rock")
+                        select g;
+
+
+            foreach (var genero in query)
+            {
+                Console.WriteLine("{0}\t{1}", genero.Id, genero.Nome);
+            }
+
+
+            Console.ReadKey();
         }
+    }
+
+    class Genero
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
     }
 }
