2d585c4922c39d9036a541c3501f89c969cb691c
Marcos Paulo <marcos.poo@gmail.com>
2024-07-31 16:56:03 -0300

    Aula 3.2 - Linq to entities where

---
+++ AluraTunes/LinqToEntities.cs
@@ -0,0 +1,41 @@
+﻿using AluraTunes.Data;
+using System;
+using System.Linq;
+
+namespace AluraTunes
+{
+    internal class LinqToEntities
+    {
+        private void exemploLinqToEntities()
+        {
+            using (var contexto = new AluraTunesEntities())
+            {
+                var query = from g in contexto.Generos
+                            select g;
+
+                foreach (var genero in query)
+                {
+                    Console.WriteLine("{0}\t{1}", genero.GeneroId, genero.Nome);
+                }
+
+                Console.WriteLine();
+
+                var queryFaixaEGenero = from g in contexto.Generos
+                                        join f in contexto.Faixas on g.GeneroId equals f.GeneroId
+                                        select new { f, g };
+
+                queryFaixaEGenero = queryFaixaEGenero.Take(10);
+
+                contexto.Database.Log = Console.WriteLine;
+
+                foreach (var item in queryFaixaEGenero)
+                {
+                    Console.WriteLine("{0}\t{1}", item.f.Nome, item.g.Nome);
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
--- AluraTunes/Program.cs
+++ AluraTunes/Program.cs
@@ -9,34 +9,21 @@ namespace AluraTunes
     {
         static void Main(string[] args)
         {
-           using(var contexto = new AluraTunesEntities())
+            using (var contexto = new AluraTunesEntities())
             {
-                var query = from g in contexto.Generos
-                            select g;
+                var textoBusca = "Led";
 
-                foreach (var genero in query) 
+                var query = from a in contexto.Artistas
+                            where a.Nome.Contains(textoBusca)
+                            select a;
+
+                foreach (var artista in query)
                 {
-                    Console.WriteLine("{0}\t{1}",genero.GeneroId, genero.Nome);
-                }
-
-                Console.WriteLine();
-
-                var queryFaixaEGenero = from g in contexto.Generos
-                                        join f in contexto.Faixas on g.GeneroId equals f.GeneroId
-                                        select new { f, g };
-
-                queryFaixaEGenero = queryFaixaEGenero.Take(10);
-
-                contexto.Database.Log = Console.WriteLine;
-
-                foreach (var item in queryFaixaEGenero)
-                {
-                    Console.WriteLine("{0}\t{1}",item.f.Nome, item.g.Nome);
+                    Console.WriteLine("{0}\t{1}",artista.ArtistaId, artista.Nome);
                 }
 
             }
-
-           Console.ReadKey();
+            Console.ReadKey();
         }
     }
 }
