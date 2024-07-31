7814c643df0becc3e796333e04a6128a49fbcd46
Marcos Paulo <marcos.poo@gmail.com>
2024-07-31 00:02:29 -0300

    Aula 2 - Linq to Xml

--- AluraTunes/Program.cs
+++ AluraTunes/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace AluraTunes
 {
@@ -10,7 +8,36 @@ namespace AluraTunes
     {
         static void Main(string[] args)
         {
-           
+            string filePath = @"C:\arquivos\AluraTunes.xml";
+
+            XElement root = XElement.Load(filePath);
+
+            var queryXML =
+                from g in root.Elements("Generos").Elements("Genero")
+                select g;
+
+            foreach (var genero in queryXML) 
+            {
+                Console.WriteLine("{0}\t{1}", genero.Element("GeneroId").Value, genero.Element("Nome").Value);
+            }
+
+            Console.WriteLine();
+
+            var query =
+                from g in root.Elements("Generos").Elements("Genero")
+                join m in root.Elements("Musicas").Elements("Musica") on g.Element("GeneroId").Value equals m.Element("GeneroId").Value
+                select new
+                {
+                    Musica = m.Element("Nome").Value,
+                    Genero = g.Element("Nome").Value
+                };
+
+            foreach (var musicaEgenero in query)
+            {
+                Console.WriteLine("{0}\t{1}", musicaEgenero.Musica, musicaEgenero.Genero);
+            }
+
+            Console.ReadKey();
         }
     }
 }
