24e9d87b58c1410eed1ee575647d7549a630f4f4
Marcos Paulo <marcos.poo@gmail.com>
2024-07-31 16:45:29 -0300

    Aula 3.1 - Linq to entities contexto join take log sql

--- AluraTunes/Program.cs
+++ AluraTunes/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AluraTunes.Data;
+using System;
 using System.Linq;
 using System.Xml.Linq;
 
@@ -8,7 +9,34 @@ namespace AluraTunes
     {
         static void Main(string[] args)
         {
-           
+           using(var contexto = new AluraTunesEntities())
+            {
+                var query = from g in contexto.Generos
+                            select g;
+
+                foreach (var genero in query) 
+                {
+                    Console.WriteLine("{0}\t{1}",genero.GeneroId, genero.Nome);
+                }
+
+                Console.WriteLine();
+
+                var queryFaixaEGenero = from g in contexto.Generos
+                                        join f in contexto.Faixas on g.GeneroId equals f.GeneroId
+                                        select new { f, g };
+
+                queryFaixaEGenero = queryFaixaEGenero.Take(10);
+
+                contexto.Database.Log = Console.WriteLine;
+
+                foreach (var item in queryFaixaEGenero)
+                {
+                    Console.WriteLine("{0}\t{1}",item.f.Nome, item.g.Nome);
+                }
+
+            }
+
+           Console.ReadKey();
         }
     }
 }
