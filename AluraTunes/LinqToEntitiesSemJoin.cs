ae8ab058d26cc6127d06c221e1a34e8af8733970
Marcos Paulo <marcos.poo@gmail.com>
2024-08-01 15:14:47 -0300

    Aula 4.3 - Linq to entities refinando consultas

---
+++ AluraTunes/LinqToEntitiesSemJoin.cs
@@ -0,0 +1,48 @@
+﻿using AluraTunes.Data;
+using System;
+using System.Linq;
+
+namespace AluraTunes
+{
+    internal class LinqToEntitiesSemJoin
+    {
+        private void exemploLinqToEntitiesSemJoin()
+        {
+            using (var contexto = new AluraTunesEntities())
+            {
+                var textoBusca = "Led";
+
+                var query = from a in contexto.Artistas
+                            join alb in contexto.Albums on a.ArtistaId equals alb.ArtistaId
+                            where a.Nome.Contains(textoBusca)
+                            select new
+                            {
+                                NomeArtista = a.Nome,
+                                NomeAlbum = alb.Titulo
+                            };
+
+                foreach (var item in query)
+                {
+                    Console.WriteLine("{0}\t{1}", item.NomeArtista, item.NomeAlbum);
+                }
+
+                Console.WriteLine();
+
+                var query2 = from alb in contexto.Albums
+                             where alb.Artista.Nome.Contains(textoBusca)
+                             select new
+                             {
+                                 NomeArtista = alb.Artista.Nome,
+                                 NomeAlbum = alb.Titulo
+                             };
+
+                foreach (var album in query2)
+                {
+                    Console.WriteLine("{0}\t{1}", album.NomeArtista, album.NomeAlbum);
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- AluraTunes/Program.cs
+++ AluraTunes/Program.cs
@@ -11,39 +11,29 @@ namespace AluraTunes
         {
             using (var contexto = new AluraTunesEntities())
             {
-                var textoBusca = "Led";
-
-                var query = from a in contexto.Artistas
-                            join alb in contexto.Albums on a.ArtistaId equals alb.ArtistaId
-                            where a.Nome.Contains(textoBusca)
-                            select new 
-                            { 
-                                NomeArtista = a.Nome,
-                                NomeAlbum = alb.Titulo
-                            };
-
-                foreach (var item in query)
-                {
-                    Console.WriteLine("{0}\t{1}",item.NomeArtista, item.NomeAlbum);
-                }
-
-                Console.WriteLine();
-
-                var query2 = from alb in contexto.Albums
-                             where alb.Artista.Nome.Contains(textoBusca)
-                             select new 
-                             { 
-                                NomeArtista = alb.Artista.Nome,
-                                NomeAlbum = alb.Titulo
-                             };
-
-                foreach (var album in query2)
-                {
-                    Console.WriteLine("{0}\t{1}", album.NomeArtista, album.NomeAlbum);
-                }
+                var buscaArtista = "Led Zeppelin";
+                var buscaAlbum = "Graffiti";
+                GetFaixas(contexto, buscaArtista, buscaAlbum);
 
             }
             Console.ReadKey();
+        }
+
+        private static void GetFaixas(AluraTunesEntities contexto, string buscaArtista, string buscaAlbum)
+        {
+            var query = from f in contexto.Faixas
+                        where f.Album.Artista.Nome.Contains(buscaArtista)
+                        select f;
+
+            if (!string.IsNullOrEmpty(buscaAlbum))
+            {
+                query = query.Where(q => q.Album.Titulo.Contains(buscaAlbum));
+            }
+
+            foreach (var faixa in query)
+            {
+                Console.WriteLine("{0}\t{1}", faixa.Album.Titulo.PadRight(40), faixa.Nome);
+            }
         }
     }
 }
